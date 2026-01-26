d210e24c9056b3b0c365f01a680b002418709a97
Mike Zhu <mzhu22@gmail.com>
2026-01-26 16:07:16 -0600

    cv with dyad split instead of timeseries

--- hr_model.py
+++ hr_model.py
@@ -3,12 +3,16 @@ from sklearn.base import BaseEstimator
 
 
 class HrModel(BaseEstimator):
+    def __init__(self, low=105, high=129):
+        self.low = low
+        self.high = high
+
     def fit(self, X, y):
         return self
 
     def predict(self, X):
         mean_hr = X["hr_moving_avg_10m"]
-        return (mean_hr > 105) & (mean_hr < 129)
+        return (mean_hr > self.low) & (mean_hr < self.high)
 
     def predict_proba(self, X):
         preds = self.predict(X)
